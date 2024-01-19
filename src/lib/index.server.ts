1881c868a30c45c3484d60d4ebc95521f85d8ee0
enchanteddev <119434133+enchanteddev@users.noreply.github.com>
2024-01-19 11:46:57 +0530

    switched to static url

--- src/lib/index.server.ts
+++ src/lib/index.server.ts
@@ -1,54 +1,54 @@
-import { SMTPClient } from 'emailjs';
+// import { SMTPClient } from 'emailjs';
 
-export let password = '';
-// export const password = "1234567890";
+// export let password = '';
+// // export const password = "1234567890";
 
-export function generateRandomString() {
-    const characters = 'abcdefghijklmnopqrstuvwxyz0123456789';
-    let randomString = '';
+// export function generateRandomString() {
+//     const characters = 'abcdefghijklmnopqrstuvwxyz0123456789';
+//     let randomString = '';
   
-    for (let i = 0; i < 13; i++) {
-        const randomIndex = Math.floor(Math.random() * characters.length);
-        randomString += characters.charAt(randomIndex);
-    }
-  
-    return randomString;
-}
-
-
-// let mail = nodemailer.createTransport({
-//     service: 'gmail',
-//     port: 465,
-//     secure: false,
-//     auth: {
-//       user: 'verify.petrichor@gmail.com',
-//       pass: process.env.emailpass
+//     for (let i = 0; i < 13; i++) {
+//         const randomIndex = Math.floor(Math.random() * characters.length);
+//         randomString += characters.charAt(randomIndex);
 //     }
-//   });
-
-const client = new SMTPClient({
-	user: 'verify.petrichor@gmail.com',
-	password: process.env.emailpass,
-	host: 'smtp.gmail.com',
-	ssl: true,
-});
+  
+//     return randomString;
+// }
 
 
-export function newP(){
-    console.log("k", process.env.emailpass)
-    password = generateRandomString();
-    console.log(`https://finance.petrichor.events/${password}`)
-    let mailOptions = {
-        from: 'verify.petrichor@gmail.com',
-        to: 'petrichor@iitpkd.ac.in, 112201015@smail.iitpkd.ac.in',
-        subject: 'Finance Link',
-        text: `https://finance.petrichor.events/${password}`
-    };
+// // let mail = nodemailer.createTransport({
+// //     service: 'gmail',
+// //     port: 465,
+// //     secure: false,
+// //     auth: {
+// //       user: 'verify.petrichor@gmail.com',
+// //       pass: process.env.emailpass
+// //     }
+// //   });
+
+// const client = new SMTPClient({
+// 	user: 'verify.petrichor@gmail.com',
+// 	password: process.env.emailpass,
+// 	host: 'smtp.gmail.com',
+// 	ssl: true,
+// });
+
+
+// export function newP(){
+//     console.log("k", process.env.emailpass)
+//     password = generateRandomString();
+//     console.log(`https://finance.petrichor.events/${password}`)
+//     let mailOptions = {
+//         from: 'verify.petrichor@gmail.com',
+//         to: 'petrichor@iitpkd.ac.in, 112201015@smail.iitpkd.ac.in',
+//         subject: 'Finance Link',
+//         text: `https://finance.petrichor.events/${password}`
+//     };
         
-    client.send(
-        mailOptions,
-        (err, message) => {
-            console.log(err || message);
-        }
-    );
-}
+//     client.send(
+//         mailOptions,
+//         (err, message) => {
+//             console.log(err || message);
+//         }
+//     );
+// }
--- src/routes/[slug]/+page.server.ts
+++ src/routes/[slug]/+page.server.ts
@@ -1,11 +1,10 @@
-import { password, newP } from '$lib/index.server';
+// import { password, newP } from '$lib/index.server';
 import type { Payment } from '$lib/types';
 import { error } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
 
 export const load: PageServerLoad = async ({ params, fetch }) => {
-    if (params.slug != password){
-        newP()
+    if (params.slug != process.env.pass){
         error(404, {message: 'Not Found'})
     }
 	const res = await fetch('http://petrichor-backend.vercel.app/internal/unvertrid')
